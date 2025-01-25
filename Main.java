31c6a0182ebb68f1a065470fdeb9a96f217264fa
no <no@no.no>
2025-01-25 20:12:20 +0200

    make Main.java layout as described in task descrption

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 import cypher.CypherTool;
-import static cypher.CypherTool.*;
 public class Main {
     public static void main(String[] args) {
-       CypherTool ct = new CypherTool();
-       System.out.println(shiftString("Hello", 13));
+    //    CypherTool ct = new CypherTool();
+       CypherTool.main(args);
+    //    System.out.println(shiftString("Hello", 13));
     }
 }                         
